34be997067754c63003e39d9237c3b01bb16fe11
Leonard392 <leorp392@gmail.com>
2024-02-09 20:23:04 -0500

    starting with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-function App() {
+const reactDescription = ["Fundamental", "Crutial", "Core"];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescription[genRandomInt(2)];
+
   return (
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
